aee9f0385b63c0bd65c2c554842c1adc184da536
DHJ <dahyeonee1324@naver.com>
2023-12-05 17:01:21 +0900

    login import

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,6 +1,24 @@
+import style from "./Login.module.css"
 
 const Login = () => {
     return (
-        <div>login</div>
+        <div className={style.container}>
+            <div className={style.logo}>DAEBBANG</div>
+            <div id="inputLogin">
+                <div className="loginFont">아이디</div>
+                <input type="text" placeholder="input your ID"></input>
+                <div className="loginFont">비밀번호</div>
+                <input type="password" placeholder="input your PW"></input>
+            </div>
+            <button>로그인</button>
+
+            <div>
+                <a href="#">아이디 찾기</a>
+                |
+                <a href="#">비밀번호 찾기</a>
+            </div>
+        </div>
     );
 }
+
+export default Login;
