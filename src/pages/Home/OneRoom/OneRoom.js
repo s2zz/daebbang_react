26fa42aa1e83d2ec9d48ac77f97b4a53317a2a47
s2zz <hey_spring@naver.com>
2023-12-06 23:57:03 +0900

    map event add

--- src/pages/Home/OneRoom/OneRoom.js
+++ src/pages/Home/OneRoom/OneRoom.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Routes, Route, Link } from "react-router-dom";
-import { Helmet } from "react-helmet";
-import { Map } from "react-kakao-maps-sdk";
+import { Routes, Route, Link, useNavigate } from "react-router-dom";
+import { Map, MapMarker, ZoomControl } from "react-kakao-maps-sdk";
+import { markerdata } from "./data/markerData"; // 마커 데이터 가져오기
 
 import List from "./List/List";
 import Info from "./Info/Info";
@@ -9,16 +9,31 @@ import Info from "./Info/Info";
 import style from "./OneRoom.module.css";
 
 function OneRoom() {
-  
-    // 지도 랜더링 될때 한 번만 실행되게
   const [mapRendered, setMapRendered] = useState(false);
+  const [zoomLevel, setZoomLevel] = useState(6);
+  const navigate = useNavigate();
+
   useEffect(() => {
     setMapRendered(true);
   }, []);
 
+  const handleDragEnd = (map) => {
+    // 이벤트가 발생하면 페이지 이동
+    navigate('/home/oneroom/list');
+  };
+
+  const handleZoomChanged = (map) => {
+    // Zoom level이 변경되면 페이지 이동
+    setZoomLevel(map.getLevel());
+    navigate(`/home/oneroom/list?zoom=${map.getLevel()}`);
+  };
+
+  const handleMarkerClick = (marker) => {
+    navigate('/home/oneroom/info', { state: marker });
+  };
+
   return (
     <div className="container">
-      <Helmet></Helmet>
       <div className={style.home_top}>
         <ul>
           <li>방 찾기</li>
@@ -27,7 +42,6 @@ function OneRoom() {
           <li>
             <Link to="list">Go to List</Link>
           </li>
-
           <li>
             <Link to="info">Go to Info</Link>
           </li>
@@ -38,13 +52,24 @@ function OneRoom() {
         <div className={style.home_body_map}>
           {mapRendered && (
             <Map
-              center={{ lat: 33.5563, lng: 126.79581 }} // 지도의 중심 좌표
-              style={{ width: "100%", height: "100%" }} // 지도 크기
-              level={3} // 지도 확대 레벨
-            ></Map>
+              center={{ lat: 36.84142696925057, lng: 127.14542099214732 }}
+              style={{ width: "100%", height: "100%" }}
+              level={zoomLevel}
+              onDragEnd={handleDragEnd}
+              onZoomChanged={handleZoomChanged}
+            >
+              {markerdata.map((marker, index) => (
+                <MapMarker
+                  key={index}
+                  position={{ lat: marker.lat, lng: marker.lng }}
+                  options={{ title: marker.title }}
+                  onClick={() => handleMarkerClick(marker)}
+                />
+              ))}
+            </Map>
           )}
         </div>
-        <div className={style.home_body_info}>
+        <div className={style.home_body_side}>
           <Routes>
             <Route path="list" element={<List />} />
             <Route path="info" element={<Info />} />
