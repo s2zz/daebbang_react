54e9fc5aaf6cde82afb237a9e0e091ec886ef1cd
DHJ <dahyeonee1324@naver.com>
2023-12-06 09:28:28 +0900

    login design update

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -3,19 +3,23 @@ import style from "./Login.module.css"
 const Login = () => {
     return (
         <div className={style.container}>
-            <div className={style.logo}>DAEBBANG</div>
-            <div id="inputLogin">
-                <div className="loginFont">아이디</div>
-                <input type="text" placeholder="input your ID"></input>
-                <div className="loginFont">비밀번호</div>
-                <input type="password" placeholder="input your PW"></input>
-            </div>
-            <button>로그인</button>
-
-            <div>
-                <a href="#">아이디 찾기</a>
-                |
-                <a href="#">비밀번호 찾기</a>
+            <div className={style.loginBox}>
+                <div className={style.logo}>DAEBBANG</div>
+                <div className={style.inputLoginBox}>
+                    <div className={style.inputLogin}>
+                        <div className={style.loginFont}>아이디</div>
+                        <input type="text" placeholder="input your ID"></input>
+                        <div className={style.loginFont}>비밀번호</div>
+                        <input type="password" placeholder="input your PW"></input>
+                    </div>
+                </div>
+                <div className={style.btnBox}>
+                    <button className={style.loginBtn}>로그인</button>
+                </div>
+                <div className={style.findBox}>
+                    <a className={style.findId} href="#">아이디 찾기</a>
+                    <a className={style.findPw} href="#">비밀번호 찾기</a>
+                </div>
             </div>
         </div>
     );
