302f7614f78820b0f26b5f29735a8a9904f2877a
SJL <zun0352@naver.com>
2023-12-06 13:09:05 +0900

    topform ui 제작

--- src/pages/commons/TopForm.js
+++ src/pages/commons/TopForm.js
@@ -1,13 +1,54 @@
-
+import style from "./TopForm.module.css"
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
+import logo from "../Enrollment/assets/logo.png";
 
 const TopForm = () => {
+
     
   return (
-    <div>
-      <Button><Link to="/enrollment">중개사무소 가입</Link></Button>
+    <div className={style.container}>
+        <div className={style.top}>
+            <div className={style.logo_wrap}>
+            <Link to="/"><img src={logo} alt="..." /> </Link>
+            </div>
+
+            <div className={style.gnb}>
+                <ul className={style.gnb_container}>
+                    <li className={style.has_d2}>
+                        <a>
+                            <Link to="/main"><span>원룸</span></Link>
+                        </a>
+                        <div className={style.depth2_bx}>
+                            <a href="#">방찾기</a>
+                            <a href="#">찜한매물</a>
+                            <a href="#">방내놓기(전월세만)</a>
+                        </div>
+                    </li>
+                    <li className={style.has_d2}>
+                        <a>
+                        <Link to="/board"> <span>게시판</span></Link>
+                        </a>
+                        <div className={style.depth2_bx}>
+                            <a href="#">자유게시판</a>
+                            <a href="#">양도게시판</a>
+                            <a href="#">작성하기</a>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div className={style.top_btn}>
+                <div className="top_btn_menu">
+                    <Button><Link to="/login">로그인 및 회원가입</Link></Button>
+                </div>
+                <div className="top_btn_menu">
+                    <Button><Link to="/enrollment">중개사무소 가입</Link></Button>
+                </div>
+            
+            </div>
+       
+    </div>
     </div>
   );
 };
