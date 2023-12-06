c19486701ea6069439d3e10951de882db79aa25e
s2zz <hey_spring@naver.com>
2023-12-06 14:37:17 +0900

    home update

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import OneRoom from '../Home/OneRoom/OneRoom';
 
+import { Map } from "react-kakao-maps-sdk";
+
+const CallMap = () => {
+<Map
+center={{ lat: 33.5563, lng: 126.79581 }} // 지도의 중심 좌표
+style={{ width: "100%", height: "100%" }} // 지도 크기
+level={3} // 지도 확대 레벨
+></Map>
+}
+
 
 
 function Home() {
--- src/pages/Home/OneRoom/OneRoom.js
+++ src/pages/Home/OneRoom/OneRoom.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import { Map } from "react-kakao-maps-sdk";
 
@@ -9,26 +9,36 @@ import Info from "./Info/Info";
 import style from "./OneRoom.module.css";
 
 function OneRoom() {
+  const [mapRendered, setMapRendered] = useState(false);
+
+  // 컴포넌트가 처음으로 렌더링될 때 한 번만 실행
+  useEffect(() => {
+    setMapRendered(true);
+  }, []);
+
   return (
     <div className="container">
-      <Helmet>
-
-      </Helmet>
+      <Helmet></Helmet>
       <div className={style.home_top}>
         <ul>
           <li>방 찾기</li>
           <li>찜한 매물</li>
           <li>방 내놓기(전월세만)</li>
+          <li><Link to="list">Go to List</Link></li>
+          
+          <li><Link to="info">Go to Info</Link></li>
         </ul>
       </div>
 
       <div className={style.main_box}>
         <div className={style.home_body_map}>
-          <Map
-            center={{ lat: 33.5563, lng: 126.79581 }} // 지도의 중심 좌표
-            style={{ width: "100%", height: "100%" }} // 지도 크기
-            level={3} // 지도 확대 레벨
-          ></Map>
+          {mapRendered && (
+            <Map
+              center={{ lat: 33.5563, lng: 126.79581 }} // 지도의 중심 좌표
+              style={{ width: "100%", height: "100%" }} // 지도 크기
+              level={3} // 지도 확대 레벨
+            ></Map>
+          )}
         </div>
         <div className={style.home_body_info}>
           <Routes>
