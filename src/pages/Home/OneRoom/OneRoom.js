95b7f8b6062d1784ed65bfbdb3d3b0475d07d31c
s2zz <hey_spring@naver.com>
2023-12-06 13:19:50 +0900

    main update

--- src/pages/Home/OneRoom/OneRoom.js
+++ src/pages/Home/OneRoom/OneRoom.js
@@ -1,19 +1,25 @@
-// OneRoom.js
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Map from "./Map/Map";
+import Item from "./Item/Item";
 
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Map from './Map/Map';
-import Item from './Item/Item';
+import style from './OneRoom.module.css';
 
 function OneRoom() {
-    return (
-        <div className="container">
-            <Routes>
-                <Route path="map" element={<Map />} />
-                <Route path="item" element={<Item />} />
-            </Routes>
+  return (
+    <div className="container">
+      <div className="home_top">방찾기 / 찜한 매물 / 방 올리기</div>
+      <div className={style.main_box}>
+        <div className="home_body_map">지도 띄울 곳</div>
+        <div className="home_body_info">
+          <Routes>
+            <Route path="map" element={<Map />} />
+            <Route path="item" element={<Item />} />
+          </Routes>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default OneRoom;
