df9bd204295b83bb53e0fca19d62e5df22e1ac3a
s2zz <hey_spring@naver.com>
2023-12-06 13:31:41 +0900

    main update

--- src/pages/Home/OneRoom/OneRoom.js
+++ src/pages/Home/OneRoom/OneRoom.js
@@ -2,15 +2,18 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Map from "./Map/Map";
 import Item from "./Item/Item";
-
 import style from './OneRoom.module.css';
+
+<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=97341ac4e80b9a87a5f181cdc1456971"></script>
 
 function OneRoom() {
   return (
     <div className="container">
       <div className="home_top">방찾기 / 찜한 매물 / 방 올리기</div>
       <div className={style.main_box}>
-        <div className="home_body_map">지도 띄울 곳</div>
+        <div className="home_body_map">
+
+        </div>
         <div className="home_body_info">
           <Routes>
             <Route path="map" element={<Map />} />
