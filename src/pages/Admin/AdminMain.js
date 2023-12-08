22a8da802dbcf61808d017f60a1e0639b4b1698a
가입dldidsla13 <dldidsla13@naver.com>
2023-12-08 17:46:14 +0900

    NG-56 공인중개사 수정

--- src/pages/Admin/AdminMain.js
+++ src/pages/Admin/AdminMain.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 const AdminMain = () => {
-    axios.get("/api/admin/userip").then((resp) => {
-        console.log(resp);
-    }).catch(err => {
-        console.log(err);
-    })
+    
     return (
         <div>
             fsf
