ae62a9bac3ea19d783effaaf5e847916a08553f1
DHJ <dahyeonee1324@naver.com>
2023-12-13 17:09:52 +0900

    admin topform update

--- src/pages/Admin/SideBar.js
+++ src/pages/Admin/SideBar.js
@@ -14,7 +14,9 @@ const SideBar = () => {
             <div>
                 <Link to="/admin/toNotificationManagement">신고 관리</Link>
             </div>
-            
+            <div>
+                <Link to="/admin/adminCRUD">관리자 등록/수정/삭제</Link>
+            </div>
         </>
     );
 }
