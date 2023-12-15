c641da274b74de79103454ce10cab2163f06abdb
DHJ <dahyeonee1324@naver.com>
2023-12-15 12:43:01 +0900

    estate mypage read&delete&pwupdate

--- src/pages/MyPage/DeleteMyInfo.js
+++ src/pages/MyPage/DeleteMyInfo.js
@@ -7,6 +7,7 @@ import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 
 const DeleteMyInfo = () => {
     const storedLoginId = sessionStorage.getItem('loginId');
+    const isEstate = sessionStorage.getItem('isEstate');
     const [pw, setPw] = useState({ pw: "" });
 
     const handleChange = (e) => {
@@ -35,26 +36,64 @@ const DeleteMyInfo = () => {
         });
     }
 
+    const handleEstateDelete = () => {
+        const formData = new FormData();
+        formData.append("id", storedLoginId);
+        formData.append("pw", pw.pw);
+        axios.post("/api/estate/login", formData).then(resp => {
+            let proceedDel = window.confirm("회원탈퇴하시겠습니까?");
+            if (proceedDel) {
+                alert("회원탈퇴가 완료되었습니다.");
+                axios.delete("/api/estate/delete/" + storedLoginId);
+                sessionStorage.removeItem('loginId');
+                sessionStorage.removeItem('isEstate');
+                navi("/");
+                window.location.reload();
+            }
+        }).catch(resp => {
+            console.log(resp);
+            alert("비밀번호를 다시 확인해주세요.");
+        });
+    }
+
     return (
         <div className={style.delContainer}>
             <Link to="/mypage"><button className={style.backBtn}><FontAwesomeIcon icon={faArrowLeft} className={style.arrowLeft} /></button></Link>
             <div className={style.titleBox}>
                 <div className={style.title}>회원 탈퇴</div>
             </div>
-            
-            <div className={style.inputBox}>
+            {!isEstate ?
                 <div>
-                    ID<br></br>
-                    {storedLoginId}<br></br><br></br>
-                    PW<br></br>
-                    <input type="password" placeholder="input your PW" name="pw" onChange={handleChange} value={pw.pw} />
+                    <div className={style.inputBox}>
+                        <div>
+                            ID<br></br>
+                            {storedLoginId}<br></br><br></br>
+                            PW<br></br>
+                            <input type="password" placeholder="input your PW" name="pw" onChange={handleChange} value={pw.pw} />
+                        </div>
+                    </div>
+                    <div className={style.delBtns}>
+                        <div>
+                            <button onClick={handleDelete} className={style.delBtn}>회원 탈퇴</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className={style.delBtns}>
+                :
                 <div>
-                    <button onClick={handleDelete} className={style.delBtn}>회원 탈퇴</button>
-                </div>
-            </div>
+                    <div className={style.inputBox}>
+                        <div>
+                            ID<br></br>
+                            {storedLoginId}<br></br><br></br>
+                            PW<br></br>
+                            <input type="password" placeholder="input your PW" name="pw" onChange={handleChange} value={pw.pw} />
+                        </div>
+                    </div>
+                    <div className={style.delBtns}>
+                        <div>
+                            <button onClick={handleEstateDelete} className={style.delBtn}>회원 탈퇴</button>
+                        </div>
+                    </div>
+                </div>}
         </div>
     );
 }
