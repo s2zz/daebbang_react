df32b20f144b3cea0c248802fba01f7c54cb156f
MHW <a@b.com>
2023-12-15 10:55:03 +0900

    댓글 수정/삭제 게시글 수정/삭제 권한

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -12,7 +12,7 @@ import FreeBoardContents from './BoardContents/js/FreeBoardContents';
 import RoomBoardContents from './BoardContents/js/RoomBoardContents';
 import EditFreeBoardContents from './EditBoardContents/js/EditFreeBoardContents';
 import EditRoomBoardContents from './EditBoardContents/js/EditRoomBoardContents';
-const Board = () => {
+const Board = ({loginId, admin}) => {
     
     return (
         <div className={style.boardContainer}>
@@ -27,8 +27,8 @@ const Board = () => {
                     <Route path="/toRoomBoardList" element={<RoomBoardList/>}  ></Route>
                     <Route path="/toRoomBoardWrite" element={<RoomBoardWrite/>}></Route>
                     <Route path="/toFreeBoardWrite" element={<FreeBoardWrite/>}></Route>
-                    <Route path="/toFreeBoardContents/*" element={<FreeBoardContents/>} ></Route>
-                    <Route path="/toRoomBoardContents/*" element={<RoomBoardContents/>}></Route>
+                    <Route path="/toFreeBoardContents/*" element={<FreeBoardContents loginId={loginId} admin={admin}/>} ></Route>
+                    <Route path="/toRoomBoardContents/*" element={<RoomBoardContents loginId={loginId} admin={admin}/>}></Route>
                     <Route path="/toEditFreeBoardContents/*" element={<EditFreeBoardContents/>}></Route>
                     <Route path="/toEditRoomBoardContents/*" element={<EditRoomBoardContents/>}></Route>
                 </Routes>
