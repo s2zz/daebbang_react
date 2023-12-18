aa61b96af8783992eaee9a844ae09a8e503adf2f
가입dldidsla13 <dldidsla13@naver.com>
2023-12-18 11:48:16 +0900

    사이드바 ui수정

--- src/pages/Admin/SideBar.js
+++ src/pages/Admin/SideBar.js
@@ -1,18 +1,19 @@
 import { Link } from 'react-router-dom';
+import style from "./SideBar.module.css";
 const SideBar = () => {
     return (
         <>
-            <div>
-                <Link to="/admin">메인페이지</Link>
+            <div className={style.menu}>
+                <Link className={style.link} to="/admin">&raquo; 메인페이지</Link>
             </div>
-            <div>
-                <Link to="/admin/toMemberManagement">회원 관리</Link>
+            <div className={style.menu}>
+                <Link className={style.link} to="/admin/toMemberManagement">&raquo; 회원 관리</Link>
             </div>
-            <div>
-                <Link to="/admin/toAgentManagement">공인중개사 관리</Link>
+            <div className={style.menu}>
+                <Link className={style.link} to="/admin/toAgentManagement">&raquo; 공인중개사 관리</Link>
             </div>
-            <div>
-                <Link to="/admin/toNotificationManagement">리뷰 관리</Link>
+            <div className={style.menu}>
+                <Link className={style.link} to="/admin/toNotificationManagement">&raquo; 리뷰 관리</Link>
             </div>
         </>
     );
