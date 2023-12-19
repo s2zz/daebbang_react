e9720cdda25c43962e93280d94b8eaee8dab4801
MHW <a@b.com>
2023-12-19 16:27:39 +0900

    리뷰 작성창 ui 수정

--- src/pages/Review/Write/WriteReview.js
+++ src/pages/Review/Write/WriteReview.js
@@ -2,7 +2,7 @@ import style from './WriteReview.module.css';
 import fav from '../assets/favorites.png';
 import notFav from '../assets/notFavorite.png';
 import { useState } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate, Link } from "react-router-dom";
 import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlus, faXmark } from '@fortawesome/free-solid-svg-icons';
@@ -10,8 +10,8 @@ import { faPlus, faXmark } from '@fortawesome/free-solid-svg-icons';
 const WriteReview = () => {
     const location = useLocation();
     const navi = useNavigate();
-    const estateId = location.state !== null && location.state.estateCode!== null ? location.state.estateCode : 0;
-    const approvalCode = location.state !== null && location.state.approvalCode!== null ? location.state.approvalCode :"";
+    const estateId = location.state !== null && location.state.estateCode !== null ? location.state.estateCode : 0;
+    const approvalCode = location.state !== null && location.state.approvalCode !== null ? location.state.approvalCode : "";
     const [formData, setFormData] = useState({ estateId: estateId, approvalCode: approvalCode, traffic: "", surroundings: "", facility: "", files: {} });
 
     const handleChange = (e) => {
@@ -38,8 +38,8 @@ const WriteReview = () => {
     }
 
     const imgDel = (files) => {
-        setUrl(prev=>({...prev,[files]:""}));
-        setFormData(prev=>({...prev,files:{...prev.files,[files]:""}}))
+        setUrl(prev => ({ ...prev, [files]: "" }));
+        setFormData(prev => ({ ...prev, files: { ...prev.files, [files]: "" } }))
     }
 
     const [score, setScore] = useState({ 0: false, 1: false, 2: false, 3: false, 4: false });
@@ -62,48 +62,48 @@ const WriteReview = () => {
     const handleAdd = () => {
         let totalScore = Object.values(score).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
         console.log(totalScore)
-        if(totalScore===0){
+        if (totalScore === 0) {
             alert("별점을 입력해주세요");
             return;
         }
 
-        if(formData.traffic===""){
+        if (formData.traffic === "") {
             alert("교통 리뷰를 입력해주세요");
             return;
         }
 
-        if(formData.surroundings===""){
+        if (formData.surroundings === "") {
             alert("주변 환경 리뷰를 입력해주세요");
             return;
         }
 
-        if(formData.facility===""){
+        if (formData.facility === "") {
             alert("시설 리뷰를 입력해주세요");
             return;
         }
 
         console.log(formData.files);
         const submitFormData = new FormData();
-        submitFormData.append("estateId",formData.estateId);
-        submitFormData.append("approvalCode",formData.approvalCode);
-        submitFormData.append("traffic",formData.traffic);
-        submitFormData.append("surroundings",formData.surroundings);
-        submitFormData.append("facility",formData.facility);
-        submitFormData.append("score",totalScore);
+        submitFormData.append("estateId", formData.estateId);
+        submitFormData.append("approvalCode", formData.approvalCode);
+        submitFormData.append("traffic", formData.traffic);
+        submitFormData.append("surroundings", formData.surroundings);
+        submitFormData.append("facility", formData.facility);
+        submitFormData.append("score", totalScore);
 
         let filesList = Object.values(formData.files);
-        
-        filesList.forEach((e)=>{
-            if(e!==""){
+
+        filesList.forEach((e) => {
+            if (e !== "") {
                 console.log(e);
-                submitFormData.append("files",e);
+                submitFormData.append("files", e);
             }
         })
-        
-        axios.post("/api/review",submitFormData).then(resp=>{
+
+        axios.post("/api/review", submitFormData).then(resp => {
             alert("리뷰 등록에 성공하였습니다");
             navi("/");
-        }).catch(err=>{
+        }).catch(err => {
             alert("리뷰 등록에 실패하였습니다");
             console.log(err);
         })
@@ -111,50 +111,54 @@ const WriteReview = () => {
     }
 
     return (
-        <>
-            <div>매물 번호 : {estateId}</div>
-            <div className={style.scoreBox}>
-                <div>
-                    {
-                        score[0] ? <img src={fav} alt="..." onClick={() => delScore(0)} /> : <img src={notFav} alt="..." onClick={() => addScore(0)} />
-                    }
-                </div>
-                <div>
-                    {
-                        score[1] ? <img src={fav} alt="..." onClick={() => delScore(1)} /> : <img src={notFav} alt="..." onClick={() => addScore(1)} />
-                    }
-                </div>
-                <div>
-                    {
-                        score[2] ? <img src={fav} alt="..." onClick={() => delScore(2)} /> : <img src={notFav} alt="..." onClick={() => addScore(2)} />
-                    }
-                </div>
-                <div>
-                    {
-                        score[3] ? <img src={fav} alt="..." onClick={() => delScore(3)} /> : <img src={notFav} alt="..." onClick={() => addScore(3)} />
-                    }
-                </div>
-                <div>
-                    {
-                        score[4] ? <img src={fav} alt="..." onClick={() => delScore(4)} /> : <img src={notFav} alt="..." onClick={() => addScore(4)} />
-                    }
+        <div className={style.borderBox}>
+            <div className={style.boardTitle}>리뷰 작성 | <span>매물 번호 : {estateId}</span></div>
+            <hr></hr>
+            
+            <div className={style.scoreInfo}>
+                <div>별점<span>*</span> &nbsp;| </div>
+                <div className={style.scoreBox}>
+                    <div>
+                        {
+                            score[0] ? <img src={fav} alt="..." onClick={() => delScore(0)} /> : <img src={notFav} alt="..." onClick={() => addScore(0)} />
+                        }
+                    </div>
+                    <div>
+                        {
+                            score[1] ? <img src={fav} alt="..." onClick={() => delScore(1)} /> : <img src={notFav} alt="..." onClick={() => addScore(1)} />
+                        }
+                    </div>
+                    <div>
+                        {
+                            score[2] ? <img src={fav} alt="..." onClick={() => delScore(2)} /> : <img src={notFav} alt="..." onClick={() => addScore(2)} />
+                        }
+                    </div>
+                    <div>
+                        {
+                            score[3] ? <img src={fav} alt="..." onClick={() => delScore(3)} /> : <img src={notFav} alt="..." onClick={() => addScore(3)} />
+                        }
+                    </div>
+                    <div>
+                        {
+                            score[4] ? <img src={fav} alt="..." onClick={() => delScore(4)} /> : <img src={notFav} alt="..." onClick={() => addScore(4)} />
+                        }
+                    </div>
                 </div>
             </div>
-            <hr />
-            <div>
-                <div>교통</div>
+            <div className={style.reviewInsert}>
+                <div>교통<span>*</span></div>
                 <div>
                     <textarea placeholder="교통 리뷰 입력" onChange={handleChange} name="traffic"></textarea>
                 </div>
             </div>
-            <div>
-                <div>주변 환경</div>
+            <div className={style.reviewInsert}>
+                <div>주변 환경<span>*</span></div>
                 <div>
                     <textarea placeholder="주변 환경 리뷰 입력" onChange={handleChange} name="surroundings"></textarea>
                 </div>
             </div>
-            <div>
-                <div>시설</div>
+            <div className={style.reviewInsert}>
+                <div>시설<span>*</span></div>
                 <div>
                     <textarea placeholder="시설 리뷰 입력" onChange={handleChange} name="facility"></textarea>
                 </div>
@@ -175,7 +179,7 @@ const WriteReview = () => {
                         {url.files0 ?
                             <>
                                 <img src={url.files0} alt="..." />
-                                <label onClick={()=>imgDel("files0")}><FontAwesomeIcon icon={faXmark} size="2xs" /></label>
+                                <label onClick={() => imgDel("files0")}><FontAwesomeIcon icon={faXmark} size="2xs" /></label>
                             </> :
                             <label htmlFor="fileInput0"><FontAwesomeIcon icon={faPlus} size="2xs" /></label>
                         }
@@ -185,7 +189,7 @@ const WriteReview = () => {
                         {url.files1 ?
                             <>
                                 <img src={url.files1} alt="..." />
-                                <label onClick={()=>imgDel("files1")}><FontAwesomeIcon icon={faXmark} size="2xs" /></label>
+                                <label onClick={() => imgDel("files1")}><FontAwesomeIcon icon={faXmark} size="2xs" /></label>
                             </> :
                             <label htmlFor="fileInput1"><FontAwesomeIcon icon={faPlus} size="2xs" /></label>
 
@@ -195,7 +199,7 @@ const WriteReview = () => {
                         {url.files2 ?
                             <>
                                 <img src={url.files2} alt="..." />
-                                <label onClick={()=>imgDel("files2")}><FontAwesomeIcon icon={faXmark} size="2xs" /></label>
+                                <label onClick={() => imgDel("files2")}><FontAwesomeIcon icon={faXmark} size="2xs" /></label>
                             </> :
                             <label htmlFor="fileInput2"><FontAwesomeIcon icon={faPlus} size="2xs" /></label>
 
@@ -205,7 +209,7 @@ const WriteReview = () => {
                         {url.files3 ?
                             <>
                                 <img src={url.files3} alt="..." />
-                                <label onClick={()=>imgDel("files3")}><FontAwesomeIcon icon={faXmark} size="2xs" /></label>
+                                <label onClick={() => imgDel("files3")}><FontAwesomeIcon icon={faXmark} size="2xs" /></label>
                             </> :
                             <label htmlFor="fileInput3"><FontAwesomeIcon icon={faPlus} size="2xs" /></label>
 
@@ -215,7 +219,7 @@ const WriteReview = () => {
                         {url.files4 ?
                             <>
                                 <img src={url.files4} alt="..." />
-                                <label onClick={()=>imgDel("files4")}><FontAwesomeIcon icon={faXmark} size="2xs" /></label>
+                                <label onClick={() => imgDel("files4")}><FontAwesomeIcon icon={faXmark} size="2xs" /></label>
                             </> :
                             <label htmlFor="fileInput4"><FontAwesomeIcon icon={faPlus} size="2xs" /></label>
 
@@ -224,11 +228,11 @@ const WriteReview = () => {
                 </div>
             </div>
             <hr />
-            <div>
-                <button>작성 취소</button>
+            <div className={style.btns}>
+                <Link to="/myPage"><button>작성 취소</button></Link>
                 <button onClick={handleAdd}>리뷰 등록</button>
             </div>
-        </>
+        </div>
     );
 }
 
