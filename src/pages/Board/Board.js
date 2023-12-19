a5334749fbbe1f95be4b597cdb160d8d8848b950
MHW <a@b.com>
2023-12-19 13:02:20 +0900

    게시판 ui 수정

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -12,27 +12,24 @@ import FreeBoardContents from './BoardContents/js/FreeBoardContents';
 import RoomBoardContents from './BoardContents/js/RoomBoardContents';
 import EditFreeBoardContents from './EditBoardContents/js/EditFreeBoardContents';
 import EditRoomBoardContents from './EditBoardContents/js/EditRoomBoardContents';
-const Board = ({loginId, admin}) => {
-    
+const Board = ({ loginId, admin }) => {
+
     return (
-        <div className={style.boardContainer}>
-             <div className={style.sideBar}>
-                <SideBar></SideBar>
-             </div>
-            <div>
-                <Routes>
-                    <Route path="/" element={<FreeBoardList/>}></Route>
-                    <Route path="/toFreeBoardList" element={<FreeBoardList/>} ></Route>
-                    <Route path="/toFavoriteBoardList" element={<FavoriteBoardList/>}  ></Route>
-                    <Route path="/toRoomBoardList" element={<RoomBoardList/>}  ></Route>
-                    <Route path="/toRoomBoardWrite" element={<RoomBoardWrite/>}></Route>
-                    <Route path="/toFreeBoardWrite" element={<FreeBoardWrite/>}></Route>
-                    <Route path="/toFreeBoardContents/*" element={<FreeBoardContents loginId={loginId} admin={admin}/>} ></Route>
-                    <Route path="/toRoomBoardContents/*" element={<RoomBoardContents loginId={loginId} admin={admin}/>}></Route>
-                    <Route path="/toEditFreeBoardContents/*" element={<EditFreeBoardContents/>}></Route>
-                    <Route path="/toEditRoomBoardContents/*" element={<EditRoomBoardContents/>}></Route>
-                </Routes>
-            </div>
+        <div className={style.boardContainer} style={{ border: "1px solid black" }}>
+            <div className={style.boardTitle}>게시판</div>
+            <hr></hr>
+            <Routes>
+                <Route path="/" element={<FreeBoardList />}></Route>
+                <Route path="/toFreeBoardList" element={<FreeBoardList />} ></Route>
+                <Route path="/toFavoriteBoardList" element={<FavoriteBoardList />}  ></Route>
+                <Route path="/toRoomBoardList" element={<RoomBoardList />}  ></Route>
+                <Route path="/toRoomBoardWrite" element={<RoomBoardWrite />}></Route>
+                <Route path="/toFreeBoardWrite" element={<FreeBoardWrite />}></Route>
+                <Route path="/toFreeBoardContents/*" element={<FreeBoardContents loginId={loginId} admin={admin} />} ></Route>
+                <Route path="/toRoomBoardContents/*" element={<RoomBoardContents loginId={loginId} admin={admin} />}></Route>
+                <Route path="/toEditFreeBoardContents/*" element={<EditFreeBoardContents />}></Route>
+                <Route path="/toEditRoomBoardContents/*" element={<EditRoomBoardContents />}></Route>
+            </Routes>
         </div >
     );
 }
