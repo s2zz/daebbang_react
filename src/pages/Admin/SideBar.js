c02599fb8f2ba723535bb301f6c00b740053780b
가입dldidsla13 <dldidsla13@naver.com>
2023-12-20 19:54:53 +0900

    공인중개사 & 공인중개사 등록 ui수정

--- src/pages/Admin/SideBar.js
+++ src/pages/Admin/SideBar.js
@@ -1,19 +1,21 @@
 import { Link } from 'react-router-dom';
 import style from "./SideBar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
 const SideBar = () => {
     return (
         <>
             <div className={style.menu}>
-                <Link className={style.link} to="/admin">&raquo; 메인페이지</Link>
+                <Link className={style.link} to="/admin"><FontAwesomeIcon icon={faAnglesRight} /> 메인 페이지</Link>
+            </div>=
+            <div className={style.menu}>
+                <Link className={style.link} to="/admin/toMemberManagement"><FontAwesomeIcon icon={faAnglesRight} /> 회원 관리</Link>
             </div>
             <div className={style.menu}>
-                <Link className={style.link} to="/admin/toMemberManagement">&raquo; 회원 관리</Link>
+                <Link className={style.link} to="/admin/toAgentManagement"><FontAwesomeIcon icon={faAnglesRight} /> 공인중개사 관리</Link>
             </div>
             <div className={style.menu}>
-                <Link className={style.link} to="/admin/toAgentManagement">&raquo; 공인중개사 관리</Link>
-            </div>
-            <div className={style.menu}>
-                <Link className={style.link} to="/admin/toNotificationManagement">&raquo; 리뷰 관리</Link>
+                <Link className={style.link} to="/admin/toNotificationManagement"><FontAwesomeIcon icon={faAnglesRight} /> 리뷰 관리</Link>
             </div>
         </>
     );
