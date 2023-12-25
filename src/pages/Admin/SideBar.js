8e24d53d6ae9cabaa4293377d9a2ee35d4fbbc59
가입dldidsla13 <dldidsla13@naver.com>
2023-12-26 08:34:16 +0900

    관리자 사이드바 수정

--- src/pages/Admin/SideBar.js
+++ src/pages/Admin/SideBar.js
@@ -1,33 +1,33 @@
 import { Link } from 'react-router-dom';
 import style from "./SideBar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
+import { faBars,faUser,faHouse,faFileAlt, faQuestion, faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 const SideBar = () => {
     return (
         <div style={{position:'fixed'}}>
             <div className={style.menu}>
-                <Link className={style.link} to="/admin"><FontAwesomeIcon icon={faAnglesRight} /> 대시보드</Link>
+                <Link className={style.link} to="/admin"><FontAwesomeIcon className={style.fontawe} icon={faBars} /> 대시보드</Link>
             </div>
             <div className={style.menu}>
-                <Link className={style.link} to="/admin/toMemberManagement"><FontAwesomeIcon icon={faAnglesRight} /> 회원 관리</Link>
+                <Link className={style.link} to="/admin/toMemberManagement"><FontAwesomeIcon className={style.fontawe} icon={faUser} /> 회원 관리</Link>
             </div>
             <div className={style.menu}>
-                <Link className={style.link} to="/admin/toAgentManagement"><FontAwesomeIcon icon={faAnglesRight} /> 공인중개사 관리</Link>
+                <Link className={style.link} to="/admin/toAgentManagement"><FontAwesomeIcon className={style.fontawe} icon={faHouse} /> 공인중개사 관리</Link>
             </div>
             <div className={style.menu}>
-                <Link className={style.link} to="#"><FontAwesomeIcon icon={faAnglesRight} /> 매물 관리</Link>
+                <Link className={style.link} to="#"><FontAwesomeIcon className={style.fontawe} icon={faFileAlt} /> 매물 관리</Link>
             </div>
             <div className={style.menu}>
-                <Link className={style.link} to="/admin/toNotificationManagement"><FontAwesomeIcon icon={faAnglesRight} /> 문의 관리</Link>
+                <Link className={style.link} to="/admin/toNotificationManagement"><FontAwesomeIcon className={style.fontawe} icon={faQuestion} /> 문의 관리</Link>
             </div>
             <div className={style.menu}>
-                <Link className={style.link} to="#"><FontAwesomeIcon icon={faAnglesRight} /> 신고 관리</Link>
+                <Link className={style.link} to="#"><FontAwesomeIcon className={style.fontawe} icon={faCircleExclamation} /> 신고 관리</Link>
             </div>
             <div className={style.menu}>
                 <Link className={style.link} to="/admin/toButtons"> 버튼</Link>
             </div>
             <div className={style.menu}>
-                <Link className={style.link} to="/admin/toAdminMain"><FontAwesomeIcon icon={faAnglesRight} /> 예전 대시보드</Link>
+                <Link className={style.link} to="/admin/toAdminMain">예전 대시보드</Link>
             </div>
         </div>
     );
