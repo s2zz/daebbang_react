eeea48cdbd11e4cdccedc3377e2650b0ba0c9079
KDH <kth5541915@gmail.com>
2023-12-27 23:42:32 +0900

    css modified

--- src/pages/EstateManage/js/SideBar.js
+++ src/pages/EstateManage/js/SideBar.js
@@ -1,20 +1,29 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import style from "../css/SideBar.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAnglesRight } from "@fortawesome/free-solid-svg-icons";
+import { faList, faPen, faHouse} from "@fortawesome/free-solid-svg-icons";
 const SideBar = () => {
+    const location = useLocation();
+
     return (
-        <div className={style.sideBar}>
+        <div style={{ position: 'fixed' }}>
             <div className={style.menu}>
-                <Link className={style.link} to="/estateManage/"><FontAwesomeIcon icon={faAnglesRight} /> 매물 관리</Link>
+                <Link className={`${style.link} ${location.pathname === '/estateManage' && style.selected}`} to="/estateManage">
+                    <FontAwesomeIcon className={style.fontawe} icon={faList} /> 매물 관리
+                </Link>
             </div>
             <div className={style.menu}>
-                <Link className={style.link} to="/estateManage/reviewApproval"><FontAwesomeIcon icon={faAnglesRight} /> 리뷰 관리</Link>
+                <Link className={`${style.link} ${location.pathname === '/estateManage/reviewApproval' && style.selected}`} to="/estateManage/reviewApproval">
+                    <FontAwesomeIcon className={style.fontawe} icon={faPen} /> 리뷰 관리
+                </Link>
             </div>
             <div className={style.menu}>
-                <Link className={style.link} to="/estateManage/estateInsert"><FontAwesomeIcon icon={faAnglesRight} /> 방 내놓기</Link>
+                <Link className={`${style.link} ${location.pathname === '/estateManage/estateInsert' && style.selected}`} to="/estateManage/estateInsert">
+                    <FontAwesomeIcon className={style.fontawe} icon={faHouse} /> 방 내놓기
+                </Link>
             </div>
         </div>
     );
 }
+
 export default SideBar;
