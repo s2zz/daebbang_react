d90e4567c3fdb3b7dd43cab23ed58c1a84a1c634
MHW <a@b.com>
2023-12-28 15:33:59 +0900

    리뷰 게시판 검색 기능

--- src/pages/Review/ReviewBoard/ReviewBoard.js
+++ src/pages/Review/ReviewBoard/ReviewBoard.js
@@ -62,8 +62,45 @@ const ReviewBoard = () => {
         const end = start + countPerPage;
         return board.slice(start, end);
     }
+    const sliceSearchContentsList = () => {
+        const start = (currentPage - 1) * countPerPage;
+        const end = start + countPerPage;
+        return searchBoard.slice(start, end);
+    }
     const currentPageHandle = (event, currentPage) => {
         setCurrentPage(currentPage);
+    }
+
+    // 검색 기능
+    const [completeSearchText, setCompleteSearchText] = useState("");
+
+    const handleSearchChange = (e) => {
+        setCompleteSearchText("");
+        setSearchText(e.target.value);
+    }
+
+    const search = () => {
+        setCurrentPage(1);
+        setCompleteSearchText(searchText);
+        searchText === "" ?
+            setSearchBoard([]) :
+            setSearchBoard(board.filter( e => e.estateId.toString().includes(searchText) || e.estate.address2.includes(searchText)));
+    }
+
+    const noBoardContents = () => {
+        return (
+            <div className={style.noBoardContents}>
+                게시글이 없습니다.
+            </div>
+        );
+    }
+
+    const contentslist = () => {
+        if (completeSearchText === "" && searchBoard.length === 0) {
+            return board.length === 0 ? noBoardContents() : sliceContentsList().map(boardItem);
+        } else {
+            return searchBoard.length === 0 ? noBoardContents() : sliceSearchContentsList().map(boardItem);
+        }
     }
 
     return (
@@ -75,11 +112,11 @@ const ReviewBoard = () => {
                     <div className={style.searchInput}>
                         <div><FontAwesomeIcon icon={faMagnifyingGlass} size="xl" style={{ color: "#535353" }} /></div>
                         <div>
-                            <input placeholder="매물 번호 또는 지역" /*onChange={handleSearchChange} value={searchText}*/ />
+                            <input placeholder="매물 번호 또는 지역" onChange={e=>handleSearchChange(e)} value={searchText} />
                         </div>
                     </div>
                     <div>
-                        <button /*onClick={() => { search() }}*/>Search</button>
+                        <button onClick={() => { search() }}>Search</button>
                     </div>
                 </div>
             </div>
@@ -92,11 +129,15 @@ const ReviewBoard = () => {
                     <div>날짜</div>
                 </div>
                 {<div className={style.boardListContents}>
-                    {sliceContentsList().map(boardItem)}
+                    {contentslist()}
                 </div>}
             </div>
             <div className={style.naviFooter}>
-                <Pagination count={Math.ceil(board.length / countPerPage)} page={currentPage} onChange={currentPageHandle} />
+                {
+                    searchBoard.length === 0 ?
+                        <Pagination count={Math.ceil(board.length / countPerPage)} page={currentPage} onChange={currentPageHandle} /> :
+                        <Pagination count={Math.ceil(searchBoard.length / countPerPage)} page={currentPage} onChange={currentPageHandle} />
+                }
             </div>
         </>
     );
